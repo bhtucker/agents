75aa27b2cfa7630e3a5710c6bdad8bf1f6b172ff
Leonardo <Leonardo Torres>
2016-05-29 16:50:31 -0400

    Feat: isolated logging/printing code. Style: better format for the printed lines.

--- compare.py
+++ compare.py
@@ -10,6 +10,21 @@ import abm_sketch as abm
 import numpy as np
 from scipy.stats.distributions import norm
 from scipy.stats.distributions import uniform
+import time
+
+
+
+def log_simulation(desc, sizes, iters, pair, lens, start, end):
+    print(desc)
+    print("-" * 70)
+    print("The clusters %r passed the same message %d times." % (sizes, iters))
+    print("The message went from %d to %d" % tuple(pair))
+    print("%d messages were successul." % len(lens))
+    print("It took %.2f +- %.2f steps (avg +- std)" % (np.average(lens), np.std(lens)))
+    print(lens)
+    print("This simulation took %.4f seconds." % (end-start))
+    print("-" * 70)
+    print("")
 
 
 
@@ -24,7 +39,7 @@ if __name__ == "__main__":
         'B': uniform(30, 50),
         'C': uniform(60, 50)
     }
-    
+
     cluster_sizes = {
         'A': 8,
         'B': 10,
@@ -32,13 +47,12 @@ if __name__ == "__main__":
     }
 
 
-    # compare how cluster sizes affects learning
+    # Compare how cluster sizes affects learning.
 
-    def run_cluster_size(sizes, iters=50):
+    def run_cluster_size(sizes, pair, iters):
         pop = abm.make_population(y_pos_dist, cluster_x_dists, sizes)
         pop.show = False
         pop.debug = False
-        pair = [0, 10]
 
         for i in range(iters):
             pop.initiate_task(fixed_pair=pair)
@@ -46,12 +60,37 @@ if __name__ == "__main__":
         return pop.success_lens
 
 
-    iters = 50
+    desc = ("Basic simulation with small clusters. The source and target\n"
+            "of the message lie in the same cluster.")
     sizes = {'A': 10, 'B': 10,'C': 10}
-    lens = run_cluster_size(sizes, iters)
+    iters = 50
+    pair = [0, 10]
+    start = time.time()
+    lens = run_cluster_size(sizes, pair, iters)
+    end = time.time()
 
-    print("The clusters %r passed the same message %d times." % (sizes, iters))
-    print("%d messages were successul." % len(lens))
-    print("It took %.2f +- %.2f steps (avg +- std)" % (np.average(lens), np.std(lens)))
-    print(lens)
+    log_simulation(desc, sizes, iters, pair, lens, start, end)
 
+
+    desc = ("One giant cluster. The source and target of the message both lie\n"
+            "in the giant cluster.")
+    sizes = {'A': 100, 'B': 10,'C': 10}
+    iters = 50
+    pair = [0, 10]
+    start = time.time()
+    lens = run_cluster_size(sizes, pair, iters)
+    end = time.time()
+
+    log_simulation(desc, sizes, iters, pair, lens, start, end)
+
+
+    desc = ("One giant cluster. The source lies within the big cluster, \n"
+            "while the target lives in one of the small ones.")
+    sizes = {'A': 100, 'B': 10,'C': 10}
+    iters = 50
+    pair = [0, 110]
+    start = time.time()
+    lens = run_cluster_size(sizes, pair, iters)
+    end = time.time()
+
+    log_simulation(desc, sizes, iters, pair, lens, start, end)
