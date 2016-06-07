18f36ca6d99811f6a04679a73663e258b88eef9b
Benson Tucker <benson@chatid.com>
2016-06-07 19:02:12 -0400

    working state after major refactor

--- abm/entities.py
+++ abm/entities.py
@@ -6,156 +6,96 @@
     Individual objects from agent world
 """
 from uuid import uuid4
-from collections import defaultdict
 from random import choice
-from scipy.stats.distributions import uniform
+
+from abm.learners import SoftmaxLearnerMixin
+
+
+class Task(object):
+    """Represents a message to be routed."""
+
+    def __init__(self, target, features=None):
+        self.target = target
+        self.id = uuid4()
+        self.value = 100
+        if features is not None:
+            self.features = features
 
 
 class Entity(object):
     """An entity in our world"""
 
-    def __init__(self, population, index):
+    def __init__(self, environment, index):
         self.index = index
-        self.population = population
+        self.environment = environment
 
-        self.sent = []    # list of points to whom I've passed the current message
         self.value = 0    # value awarded for a successful message
 
-        # extended adjacency list (see pass_message() and learn())
-        try:
-            self.adjacencies = []
-        except AttributeError as e_:
-            # subclasses may define adjacencies as a property, that's okay
-            if 'adjacencies' not in dir(self):
-                raise e_
-
-        # task_attempt_map[task_id] hold a list of the neighbors to whom
-        # this entity gave the task
-        self.task_attempt_map = defaultdict(lambda: [index])
-
     def log(self, msg):
-        self.population.log(msg)
+        self.environment.log(msg)
 
-    def receive_task(self, task, sender):
+    def _get_next_recipient(self, task):
         """
-        Handles a message received. Calls pass_message(). Use
-        sender=None if this is the Entity that receives the original
-        message.
+        Returns the node index that this entity would like to hand message to
+        May enter a 'pending' state recording this decision for future feedback
         """
+        raise NotImplementedError
 
-        self.log("%d: past receipients are " % (self.index,) + str(self.sent))
-
-        if sender:
-            if sender not in self.task_attempt_map[task.id]:
-                self.task_attempt_map[task.id].append(sender)
-
-            if sender in self.sent:
-                self.log('popping %s' % self.sent.pop(self.sent.index(sender)))
-
-        # If I have passed the message to all of my neighbors and I still
-        # received it back, bail out.
-        if set(self.adjacencies).issubset(set(self.task_attempt_map[task.id])):
-            # FIXME: this catches cycles eventually, but also catches randomly
-            # followed closed paths
-            self.log('I have passed the message to all of my neighbors, so I quit')
-            self.population.clear()
-            return
-
-        if self.index == task.target:
-            self.log('message delivered!')
-        else:
-            self.pass_message(task, sender)
-
-    def pass_message(self, task, sender):
+    def next(self, task, sender):
         """
-        If this Entity knows the target, send the message directly.
-        Otherwise, choose randomly among the (extended) adjacency list.
-        Calls Population.pass_message() with the chosen target.
+        Called by environment to get the next step in the path
         """
+        next_recipient = self._get_next_recipient(task)
 
-        next_recipient = task.target if task.target in self.adjacencies else self.index
-
-        while next_recipient in self.task_attempt_map[task.id]:
-            next_recipient = choice(self.adjacencies)
-
-        self.task_attempt_map[task.id].append(next_recipient)
         self.log("%s -> %s" % (self.index, next_recipient))
-
-        self.sent.append(next_recipient)
-        self.population.pass_message(next_recipient, task, self.index)
-
-    def set_adjacencies(self, adjacencies):
-        """
-        Builds the adjacency list for this Entity. 
-        :param adjacencies: set of integers referencing other entities in self.population
-        """
-        self.adjacencies = list(adjacencies)
+        return next_recipient
 
     def award(self, value):
         """
-        Accepts <value> as award after having participated in the
-        successful routing of a message. Calls self.learn() and then
-        cleans up to prepare for the next message.
+        Accepts <value> as award to register the outcome of recent decision(s).
+        Calls self._learn() and then cleans up to prepare for the next message.
         """
+        self.value = value
+        self.log("""
+            Node {ix} receiving award {award}
+            """.format(ix=self.index, award=value))
 
-        self.value += value
-        self.log("")
-        self.log("awarding %.2f to %d" % (self.value, self.index))
-        self.log("Map: " + str(dict(self.task_attempt_map)))
-        self.log("Adj before: " + str(self.adjacencies))
-
-        self.learn()
+        self._learn()
 
         self.value = 0
-        self.sent = []
 
-    def learn(self):
-        """Learns about the world after receiving an award."""
-
-        # After an award, we choose some adjacencies and increase the
-        # likelihood of their being chosen for a message next time
-        # around.
-
-        # The probability of choosing an adjacency to increase its
-        # likelihood is proportional to the value of the award received.
-
-        # Since at every step we choose at random from our list of
-        # adjacencies, to increase the likelihood of one of them being
-        # chosen, we need only add them again to this list, i.e., if my
-        # adjacencies are [1,2,3] and sending a message to 2 was
-        # successful, our new extended adjacency list is [1,2,3,2], and
-        # when we choose uniformly from this list, we will be more
-        # likely to choose 2 again.
-
-        # self.log("learning on %d" % self.index)
-
-        for adj in self.sent:
-            assert adj in self.adjacencies
-
-            u_val = uniform.rvs(0, 100, 1)
-            # self.log("u_val is %d, learn? %r" % (u_val, u_val < self.value))
-            if u_val < self.value:
-                self.adjacencies.append(adj)
-
-        self.log("Adj after:  " + str(self.adjacencies))
+    def _learn(self):
+        """
+        Update node's brain, however that is implemented in subclass.
+        May resolve a 'pending' state by taking the current self.value as its outcome
+        """
+        raise NotImplementedError
 
 
 class XyEntity(Entity):
     """An entity in x-y world"""
 
-    def __init__(self, population, index, x, y, cluster):
-        super(XyEntity, self).__init__(population, index)
+    def __init__(self, environment, index, x, y, cluster):
+        super(XyEntity, self).__init__(environment, index)
         self.x = x
         self.y = y
         self.cluster = cluster
+        self.adjacencies = []
+
+    def set_adjacencies(self, adjacencies):
+        """
+        Builds the adjacency list for this Entity.
+        :param adjacencies: set of integers referencing other entities in self.environment
+        """
+        self.adjacencies = list(adjacencies)
 
 
 class NxEntity(Entity):
     """An entity that can be a Nx graph node
     It can have more freeform attributes and doesn't need x, y positions for viz
     """
-    def __init__(self, population, index, **kwargs):
-        super(NxEntity, self).__init__(population, index)
+    def __init__(self, environment, index, **kwargs):
+        super(NxEntity, self).__init__(environment, index)
         self.update(kwargs)
 
     def update(self, kwargs):
@@ -167,20 +107,28 @@ class NxEntity(Entity):
 
     @property
     def adjacencies(self):
-        if self.population and self.population.graph:
-            return self.population.graph.neighbors(self.index)
+        """
+        List connected nodes via the parent graph
+        """
+        if self.environment and self.environment.graph:
+            return self.environment.graph.neighbors(self.index)
         else:
             return []
-   
-    # def __getitem__(self, attr):
-    #     return getattr(self, attr)
 
 
+class DunceNode(NxEntity):
+    """
+    Nx compatible node that doesn't learn and just randomly draws its recipients
+    """
+    def _get_next_recipient(self, task):
+        return choice(self.adjacencies)
 
-class Task(object):
-    """Represents a message to be routed."""
+    def _learn(self):
+        pass
 
-    def __init__(self, target):
-        self.target = target
-        self.id = uuid4()
-        self.value = 100
+
+class SoftmaxNode(SoftmaxLearnerMixin, NxEntity):
+    """
+    Nx nodes with softmax learning
+    """
+    pass
--- abm/pops.py
+++ abm/pops.py
@@ -3,31 +3,11 @@
     abm.pops
     ~~~~~~~~
 
-    Populations of interconnected agents
+    Environments of interconnected agents
 """
-from scipy.stats.distributions import norm
-from scipy.stats.distributions import uniform
-from sklearn.metrics.pairwise import euclidean_distances
-
-from abm.viz import display_network
-from abm.entities import Entity, Task
+from abm.entities import Task
+from cached_property import cached_property
 import numpy as np
-from random import choice
-
-
-Y_DIST = norm(300, 10)
-
-CLUSTER_X_DIST_MAP = {
-    'A': uniform(0, 50),
-    'B': uniform(30, 50),
-    'C': uniform(60, 50)
-}
-
-CLUSTER_SIZES = {
-    'A': 8,
-    'B': 10,
-    'C': 8
-}
 
 
 def make_points(cluster, size, y_dist, x_dist):
@@ -38,45 +18,40 @@ def make_points(cluster, size, y_dist, x_dist):
     return list(zip(xs, ys, [cluster] * size))
 
 
-class Population(object):
-    def __init__(self, debug=True):
+class Environment(object):
+    def __init__(self, debug=True, path_cutoff=20):
         self.path = []
         self.show = True
         self.debug = debug
         self.success_lens = []
+        self.path_cutoff = path_cutoff
 
     def log(self, msg):
         """Prints a message to stdout, only if self.debug=True."""
         if self.debug:
             print(msg)
 
-    def pass_message(self, recipient, task, sender):
-        """
-        Displays current state and routes the message one connection
-        down. Handles the case where the recipient is the target. Calls
-        Entity.receive_task(). <recipient> is the index of the recipient
-        Entity.
-        """
+    def _distribute_awards(self, task):
+        awarded = set()
+        for point in self.path:
+            if point in awarded:
+                continue
+            awarded.add(point)
+            amount = self._calculate_award(task, self.path, point)
+            self.population[point].award(amount)
 
-        self.display(recipient, task.target)
-        self.path.append(recipient)
-        self.points[recipient].receive_task(task, sender)
+        self.success_lens.append(len(self.path))
+        self.path = []
 
-        if recipient == task.target:
-            for point in self.path:
-                amount = self.calculate_award(task, self.path, point)
-                self.points[point].award(amount)
-
-            self.success_lens.append(len(self.path))
-            self.path = []
-
-    def calculate_award(self, task, path, entity):
+    def _calculate_award(self, task, path, entity):
         """
         Returns the amount awarded to <entity> after <task> has been
         routed through <path>.
         """
-        k = k = float(len(path))
-        return (task.value / k) + ((k - path.index(entity)) * 5)
+        if len(self.path) > self.path_cutoff:
+            return 0.
+        k = float(len(path))
+        return (task.value / k)
 
     def initiate_task(self, fixed_pair=None):
         """Initializes a task, and calls pass_message()."""
@@ -87,244 +62,66 @@ class Population(object):
             self.log("changing fixed pair: they're the same!")
 
         while start == end:
-            [start, end] = np.random.randint(self.size, size=2).tolist()
+            [start, end] = self._pick_start_end()
 
         self.log('new task created')
         self.log('starting at %s and aiming for %s' % (start, end))
 
-        task = Task(end)
-        self.pass_message(start, task, None)
+        task = self._generate_task(end)
+        self._run_task(start, task)
+
+    def _generate_task(self, target):
+        return Task(target)
+
+    def _run_task(self, start, task):
+
+        recipient = self.population[start].next(task, sender=None)
+        sender = start
+        self.path.append(start)
+
+        while not (recipient == task.target or len(self.path) > self.path_cutoff):
+            self.path.append(recipient)
+            next_recipient = self.population[recipient].next(task, sender=sender)
+            sender = recipient
+            recipient = next_recipient
+
+        self._distribute_awards(task)
+
+    def _pick_start_end(self):
+        return np.random.randint(self.size, size=2).tolist()
 
     def clear(self):
         """
         Clear every Entity's history. Called when the package is stuck
         in a cycle.
         """
-        point_iterator = self.points if isinstance(self.points, list) else self.points.values()
+        point_iterator = (self.population
+                          if isinstance(self.population, list)
+                          else self.population.values())
+
         for point in point_iterator:
             point.sent = []
 
 
-class XyPopulation(Population):
-    """
-    A set of connected Entities. Handles message passing and displaying.
-    Entities are connected randomly.
-    """
+class TaskFeatureMixin(object):
+    """Overrides _generate_task with a feature-defining version"""
+    @cached_property
+    def _attribute_categories(self):
+        return [(k, v.keys()) for k, v in self.attributes.items()]
 
-    def __init__(self, y_pos_dist=Y_DIST, cluster_x_dists=CLUSTER_X_DIST_MAP,
-                 cluster_sizes=CLUSTER_SIZES, single_component=True, debug=True):
-        self.points = []
-        self.path = []
-        self.show = True
-        self.debug = debug
-        self.success_lens = []
-        self.connectivity_matrix = None
-        self.connected_components = []
-        self.node_component_map = {}
-
-        self._set_entities(y_pos_dist, cluster_x_dists, cluster_sizes)
-        self._set_connectivity_matrix()
-        self._set_connections()
-        if single_component:
-            self._ensure_single_component()
-
-    def _set_entities(self, y_pos_dist, cluster_x_dists, cluster_sizes):
-        point_args = []
-        for cluster, size in cluster_sizes.iteritems():
-            point_args += make_points(cluster, size,
-                                      y_pos_dist, cluster_x_dists[cluster])
-
-        for ix, args in enumerate(point_args):
-            pt = Entity(self, ix, *args)
-            self.points.append(pt)
-        self.size = len(self.points)
-
-    def _set_connections(self, track_components=True):
-        """Initializes each Entity's adjacency list.
-        :param track_components: Flag for tracking connected components during graph construction
+    def _generate_task(self, target):
         """
-
-        self._set_connectivity_matrix()
-        for index, point in enumerate(self.points):
-
-            # make set of connections to indices; np.where returns a tuple
-            adjacencies = set(np.where(self.connectivity_matrix[index] > 0)[0])
-            adjacencies.discard(index)
-
-            # pass adjacency information down to agent
-            point.set_adjacencies(adjacencies)
-
-            if track_components:
-                # track connected components as we construct edges
-                if index in self.node_component_map:
-                    component = self.node_component_map[index]
-                else:
-                    component = set([index])
-                    self.node_component_map[index] = component
-                    self.connected_components.append(component)
-                # update the component in place with potential new members
-                component.update(adjacencies)
-
-                # update the node - component map so we can fetch this object
-                # for adjacencies
-                self.node_component_map.update(
-                    {a: component for a in adjacencies})
-
-                # resolve potential component connections
-                self._resolve_components(component)
-
-        n = float(len(self.points))
-        k = float(np.sum(self.connectivity_matrix)) / 2
-        self.edge_density = k / (n * (n - 1) / 2)
-
-    def _ensure_single_component(self):
+        Convert categorical data about the target into an indicator vector
+        Return a task with this vector accessible as task.features
         """
-        Iterate through disjoint component list, adding connections between sequential components
-        Update other datastructures to reflect the new connections
-        """
-        for ix, component in enumerate(self.connected_components[:-1]):
-            start, end = (choice(list(component)), choice(
-                list(self.connected_components[ix + 1])))
-            self.points[start].adjacencies.append(end)
-            self.points[end].adjacencies.append(start)
-            self.connectivity_matrix[start][end] = True
-            self.connectivity_matrix[end][start] = True
-            self.connected_components[ix].add(end)
-            self.connected_components[ix + 1].add(start)
+        target_node = self.population[target]
+        feature_vec_components = []
+        for attribute, categories in self._attribute_categories:
+            component = np.zeros(len(categories))
+            target_val = getattr(target_node, attribute)
+            component[categories.index(target_val)] = 1
+            feature_vec_components.append(component)
 
-        self._resolve_components(self.connected_components[0])
-
-    def _resolve_components(self, component):
-        """
-        Find components thought to be separate that now have intersections
-        Condense these and set self.connected_components to be a list of disjoint sets 
-        """
-        resolved_components = [component]
-        for other_component in self.connected_components:
-            if other_component.intersection(component) or other_component is component:
-                component.update(other_component)
-                self.node_component_map.update(
-                    {a: component for a in other_component})
-            else:
-                resolved_components.append(other_component)
-        self.connected_components = resolved_components
-
-    def _set_connectivity_matrix(self):
-        """
-        Computes the connectivity matrix of this Population. Each point is
-        connected to each other within a radius.
-        """
-
-        if self.connectivity_matrix is not None:
-            return
-
-        # generate a random symmetric matrix
-        point_count = len(self.points)
-        matrix = np.random.randint(
-            0, 2, point_count ** 2).reshape(point_count, point_count)
-        matrix = (matrix + matrix.T) / 2
-        for i in range(point_count):
-            matrix[i][i] = 0
-
-        self.connectivity_matrix = matrix
-
-    def display(self, current=None, target=None):
-        """
-        Plots the state of the task. If <show> = False, doesn't plot
-        anything and the simulation can run faster.
-        """
-        if not self.show:
-            return
-
-        display_network(self.points, self.connectivity_matrix,
-                        current=current, target=target)
-
-
-class CappedPreferentialPopulation(Population):
-    """
-    A set of connected Entities. Handles message passing and displaying. Connections are laid
-    out such that entities of the same cluster are more likely to be tied together,
-    proportionally to a parameter alpha. The overall density of the network is controlled
-    by a parameter beta.
-    """
-
-    def __init__(self, alpha=0.8, beta=0.4, *args, **kwargs):
-        self.alpha = alpha
-        self.beta = beta
-        super(CappedPreferentialPopulation, self).__init__(*args, **kwargs)
-
-    def _set_connectivity_matrix(self):
-        """
-        Computes the connectivity matrix of this Population. Each point is
-        connected to each other within a radius.
-        """
-
-        if self.connectivity_matrix is not None:
-            return
-
-        def decide_connection(point1, point2):
-            # A point is connected to another point of its same cluster
-            # with high probability proportional to alpha, and to
-            # another point of a different clluester with probability
-            # proportional to 1 - alpha.
-            # Moreover, the edge density of a network is capped at a value
-            # beta. That's why we choose a 0 with probability 1-beta,
-            # and partition beta into alpha and 1-alpha.
-
-            alpha = self.alpha
-            beta = self.beta
-
-            if point1.cluster == point2.cluster:
-                tie = np.random.choice(
-                    [0, 0, 1], p=[1 - beta, beta * (1 - alpha), beta * alpha])
-            else:
-                tie = np.random.choice(
-                    [0, 0, 1], p=[1 - beta, beta * alpha, beta * (1 - alpha)])
-            return tie
-
-        matrix = np.array([[0] * len(self.points)
-                           for _ in range(len(self.points))])
-
-        # since the graph is undirected, the matrix is symmetric,
-        # which in turn means we need only compute the lower triangular
-        # elements and then copy them into the upper triangular elements
-        for i, point1 in enumerate(self.points):
-            for j, point2 in enumerate(self.points[:i]):
-                matrix[i][j] = decide_connection(point1, point2)
-                matrix[j][i] = matrix[i][j]
-
-        self.connectivity_matrix = matrix
-
-
-class NearestNeighborsPopulation(Population):
-    """
-    A set of connected Entities. Handles message passing and displaying. Connections laid
-    out geographically: each point is connected to some of its nearest neighbors.
-    """
-
-    def _set_connectivity_matrix(self):
-        """
-        Computes the connectivity matrix of this Population. Each point is
-        connected to each other within a radius.
-        """
-
-        if self.connectivity_matrix is not None:
-            return
-
-        points_arr = np.array([[p.x, p.y] for p in self.points])
-        distance_mat = euclidean_distances(points_arr, points_arr)
-
-        # Every point p will be connected to each other point whose distance
-        # to p is less than a cut-off value. This value is computed as the
-        # mean of {min_nonzero(dist_mat(p)) | p is a point}, times a factor
-        min_nonzero = lambda r: min(r[r > 0])
-
-        # apply_along_axis(f, axis=1, arr) applies f to each row
-        min_neighbor_distances = np.apply_along_axis(
-            min_nonzero, axis=1, arr=distance_mat)
-
-        factor = 2.2
-        neighbor_cutoff = np.mean(min_neighbor_distances) * factor
-        connectivity_matrix = distance_mat < neighbor_cutoff
-
-        self.connectivity_matrix = connectivity_matrix
+        feature_vec = np.hstack(feature_vec_components)
+        task = Task(target, features=feature_vec)
+        return task
