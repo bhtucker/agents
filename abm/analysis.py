6781381b913580beeec15f96e8fc52627ac263e0
Benson Tucker <bhtucker@users.noreply.github.com>
2016-06-19 15:10:08 -0400

    update plot_learning_df
    to support callers like the initial analysis nb

--- abm/analysis.py
+++ abm/analysis.py
@@ -167,7 +167,8 @@ def plot_segment_stats(segments,
         sns.tsplot(data=grouped_time_samples.T, color=color)
 
 
-def plot_learning_df(df):
+def plot_learning_df(df, key='li', **kwargs):
     segments = segment_learning_df(df)
-    plot_segment_stats(segments)
+    segments = [k for k, v in kwargs.items() if v]
+    plot_segment_stats(segments, measure_key=key, segment_keys=segments)
     return segments
