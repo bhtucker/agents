e43de6e75380280b73620f30b8cfd696f1a9f32f
Leonardo <Leonardo Torres>
2016-06-19 20:09:29 -0400

    Style: moving from tabs to spaces.

--- tests/abm/test_nxpops.py
+++ tests/abm/test_nxpops.py
@@ -12,31 +12,34 @@ from abm import nxpops
 
 @pytest.mark.unit
 def test_feature_vec_options(simple_pop_kwargs):
-	smpop = nxpops.SoftmaxNxEnvironment(**simple_pop_kwargs)
-	task1 = smpop._generate_task(1, 2)
-	assert task1.features.sum() == 1
-	smpop = nxpops.SoftmaxNxEnvironment(bias=True, **simple_pop_kwargs)
-	task2 = smpop._generate_task(1, 2)
-	assert task2.features.sum() == 2
-	smpop = nxpops.SoftmaxNxEnvironment(bias=True, node_index_indicator=True, **simple_pop_kwargs)
-	task3 = smpop._generate_task(1, 2)
-	assert task3.features.sum() == 3
-	assert len(task2.features) == (len(task3.features) - simple_pop_kwargs['size'])
+    smpop = nxpops.SoftmaxNxEnvironment(**simple_pop_kwargs)
+    task1 = smpop._generate_task(1, 2)
+    assert task1.features.sum() == 1
+    smpop = nxpops.SoftmaxNxEnvironment(bias=True, **simple_pop_kwargs)
+    task2 = smpop._generate_task(1, 2)
+    assert task2.features.sum() == 2
+    smpop = nxpops.SoftmaxNxEnvironment(bias=True, node_index_indicator=True, **simple_pop_kwargs)
+    task3 = smpop._generate_task(1, 2)
+    assert task3.features.sum() == 3
+    assert len(task2.features) == (len(task3.features) - simple_pop_kwargs['size'])
 
 
 @pytest.mark.integration
 def test_performance_gains(fine_grained_pop_kwargs):
-	smpop = nxpops.SoftmaxNxEnvironment(**fine_grained_pop_kwargs)
-	smpop.debug = False
-	smpop.show = False
-	early_lens = []
-	for i in range(100):
-		smpop.run_task(smpop.initiate_task())
-		early_lens.append(len(smpop.path))
-	for i in range(10000):
-		smpop.run_task(smpop.initiate_task())
-	late_lens = []
-	for i in range(100):
-		smpop.initiate_task()
-		late_lens.append(len(smpop.path))
-	assert sum(early_lens) > (sum(late_lens) * 1.2)
+    smpop = nxpops.SoftmaxNxEnvironment(**fine_grained_pop_kwargs)
+    smpop.debug = False
+    smpop.show = False
+    early_lens = []
+    for _ in range(100):
+        smpop.initiate_task()
+        smpop.run_task()
+        early_lens.append(len(smpop.path))
+    for _ in range(10000):
+        smpop.initiate_task()
+        smpop.run_task()
+    late_lens = []
+    for _ in range(100):
+        smpop.initiate_task()
+        smpop.run_task()
+        late_lens.append(len(smpop.path))
+    assert sum(early_lens) > (sum(late_lens) * 1.2)
